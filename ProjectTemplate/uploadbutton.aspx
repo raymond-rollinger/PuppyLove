9d6fc8db5f0fa7b49ff8f438cf6ca20c22004e2a
Raymond <rprollin@gmail.com>
2020-02-26 20:20:31 -0700

    Separate asmx for admin functions,+viewaccounts pg

---
+++ ProjectTemplate/uploadbutton.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" Inherits="ProjectTemplate.upload_button" %>
+<!DOCTYPE html>
+<html>
+<head runat="server">
+	<title>upload_button</title>
+</head>
+<body>
+     <form id="form1" runat="server">
+        <fieldset>
+            <legend>
+                Upload Profile Photo
+            </legend>
+        <asp:FileUpload ID="Uploader" runat="server" accept="image/*" />
+        <asp:Button ID="UploadSubmit" runat="server" OnClick="UploadSubmit_Click" Text="Upload" Height="40px" BackColor="White" BorderStyle="None" ForeColor="Blue" />
+        </fieldset>
+        <asp:Label ID="StatusLabel" runat="server" Font-Italic="True"></asp:Label>
+        <br />
+    </form>
+</body>
+</html>
---
+++ ProjectTemplate/uploadbutton.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Web;
+using System.Web.UI;
+
+namespace ProjectTemplate
+{
+
+    public partial class upload_button : System.Web.UI.Page
+    {
+		protected void Page_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		protected void UploadSubmit_Click(object sender, EventArgs e)
+		{
+			if (Uploader.HasFile)
+			{
+				try
+				{
+					string filename = Path.GetFileName(Uploader.FileName);
+					Uploader.SaveAs(Server.MapPath("~/") + "Image/" + filename);
+					string[] textLines = File.ReadAllLines(Server.MapPath("~/") + "/Image/" + filename);
+
+
+					StatusLabel.Text = "Success! First line of text: " + textLines[0];
+
+
+				}
+				catch (Exception ex)
+				{
+					StatusLabel.Text = "Error: " + ex.Message;
+				}
+			}
+		}
+	}
+}
